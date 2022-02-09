5eea548cf53b8248dc52b2eddf43a3c0adb451e7
mohan12Jr <80115806+Mohan12Jr@users.noreply.github.com>
2022-02-09 13:31:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const msRest = require("@azure/ms-rest-js");
+const Face = require("@azure/cognitiveservices-face");
+const uuid = require("uuid/v4");
+
+key = "cf8d97c50e1a4c9694836c020835f280"
+endpoint = "https://rejnewi.cognitiveservices.azure.com/"
